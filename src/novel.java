156c40b60f853ff2d27054a4d426410cac90307d
Syeda Sarah Parvin <sarahsuhana2211@gmail.com>
2026-03-14 20:42:16 +0530

    Update novel.java

--- src/novel.java
+++ src/novel.java
@@ -1,55 +1,21 @@
 package model;
 
-public abstract class Book {
+public class Novel extends Book {
 
-    private String title;
-    private String author;
-    private int totalPages;
-    private int pagesRead;
-    private String status; // Planned, Reading, Completed
+    private String genre;
 
-    public Book(String title, String author, int totalPages) {
-        this.title = title;
-        this.author = author;
-        this.totalPages = totalPages;
-        this.pagesRead = 0;
-        this.status = "Planned";
+    public Novel(String title, String author, int totalPages, String genre) {
+        super(title, author, totalPages);
+        this.genre = genre;
     }
 
-    public void updateProgress(int pages) {
-        pagesRead = pages;
-
-        if (pagesRead >= totalPages) {
-            status = "Completed";
-        } else if (pagesRead > 0) {
-            status = "Reading";
-        }
+    public String getGenre() {
+        return genre;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getPagesRead() {
-        return pagesRead;
-    }
-
-    public int getTotalPages() {
-        return totalPages;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
+    @Override
     public void displayInfo() {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
-        System.out.println("Progress: " + pagesRead + "/" + totalPages);
-        System.out.println("Status: " + status);
+        super.displayInfo();
+        System.out.println("Genre: " + genre);
     }
 }
