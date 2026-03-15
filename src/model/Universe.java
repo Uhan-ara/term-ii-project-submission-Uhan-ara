beddec65e91c031bb868dfc9fb0390c0bfa52838
Syeda Sarah Parvin <sarahsuhana2211@gmail.com>
2026-03-15 08:11:39 +0530

    Update Universe.java

--- src/model/Universe.java
+++ src/model/Universe.java
@@ -1,34 +1,38 @@
 package model;
 
-import java.util.ArrayList;
+import java.util.Hashmap;
 
 public class Universe {
 
     private String name;
-    private ArrayList<Series> seriesList;
+    private Hashmap<Series> seriesList;
 
     public Universe(String name) {
         this.name = name;
-        this.seriesList = new ArrayList<>();
+        seriesList = new Hashmap<>();
     }
 
     public void addSeries(Series series) {
-        seriesList.add(series);
+        seriesList.put(series.getName(), series);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public ArrayList<Series> getSeriesList() {
-        return seriesList;
+    public Series getSeries(String name) {
+        return seriesList.get(name);
     }
 
     public void displaySeries() {
         System.out.println("Universe: " + name);
 
-        for (Series s : seriesList) {
+        for (Series s : seriesList.values()) {
             System.out.println("- " + s.getName());
         }
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public HashMap<String, Series> getSeriesList() {
+        return seriesList;
     }
 }
