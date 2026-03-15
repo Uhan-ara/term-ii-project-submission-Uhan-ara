2cb36589019fa68e6736ce6d5b1ca006d2c6416a
Syeda Sarah Parvin <sarahsuhana2211@gmail.com>
2026-03-15 07:55:00 +0530

    Add files via upload

---
+++ src/management/LibraryManager.java
@@ -0,0 +1,42 @@
+package management;
+
+import model.*;
+import java.util.ArrayList;
+
+public class LibraryManager {
+
+    private ArrayList<Universe> universes;
+
+    public LibraryManager() {
+        universes = new ArrayList<>();
+    }
+
+    public void addUniverse(Universe universe) {
+        universes.add(universe);
+    }
+
+    public void showUniverses() {
+        for (Universe u : universes) {
+            System.out.println(u.getName());
+        }
+    }
+
+    public void searchBook(String title) {
+
+        for (Universe u : universes) {
+            for (Series s : u.getSeriesList()) {
+                for (Book b : s.getBooks()) {
+
+                    if (b.getTitle().equalsIgnoreCase(title)) {
+                        System.out.println("Book found:");
+                        b.displayInfo();
+                        return;
+                    }
+
+                }
+            }
+        }
+
+        System.out.println("Book not found.");
+    }
+}
